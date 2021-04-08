16c66cd96143bce6dc9515e0d2a8bdb9c76e63be
Duncan Campbell <duncan.campbell@rabobank.nl>
2021-04-08 16:53:15 +0200

    feat: init commit o.b.v start.spring.io zipje met benodigde dependencies

---
+++ assignment/src/test/java/nl/sourcelabs/interview/duncan/assignment/InterviewAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.sourcelabs.interview.duncan.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
